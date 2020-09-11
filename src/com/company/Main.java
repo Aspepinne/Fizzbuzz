cc25e1500b39b937cd4fc939f8c41702c9f8d981
Maria Hermansson <maria.hermansson@student.kyh.se>
2020-09-11 22:45:32 +0200

    Messed up last commit, fixed it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,23 +9,14 @@ public class Main {
                 System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
             }
             else if (i % 3 == 0 && i%5 == 0){
-<<<<<<< HEAD
+                
                 System.out.println( i + " Fizzbuzz");
             }
             else if (i % 3 == 0){
                 System.out.println(i + " Fizz");
             }
-            else if (i % 5 == 0){
+            else if (i % 5 == 0) {
                 System.out.println(i + " Buzz");
-=======
-                System.out.println("Fizzbuzz");
-            }
-            else if (i % 3 == 0){
-                System.out.println("Fizz");
-            }
-            else if (i % 5 == 0){
-                System.out.println("Buzz");
->>>>>>> 5a37168dbbf4b77cdf6b688b5d23357f71c59d2c
             }
             else{
                 System.out.println(i);
