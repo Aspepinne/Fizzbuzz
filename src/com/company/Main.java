399db9e8ed5ebd4f2db3e93a6c737f610472ae0e
Maria Hermansson <maria.hermansson@student.kyh.se>
2020-09-11 22:34:21 +0200

    Changed float to int. Added blank space before Fizz, Buzz and Fizzbuzz. Added sentence at the end :)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,24 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        for (float i = 1f; i < 101f; i++) {
+        for (int i = 1; i < 101; i++) {
 
             if (i == 42){
                 System.out.println(i + " Answer to the Ultimate Question of Life, the Universe, and Everything");
             }
             else if (i % 3 == 0 && i%5 == 0){
-                System.out.println(i/3 + "Fizzbuzz");
+                System.out.println( i + " Fizzbuzz");
             }
             else if (i % 3 == 0){
-                System.out.println(i/3 + "Fizz");
+                System.out.println(i + " Fizz");
             }
             else if (i % 5 == 0){
-                System.out.println(i/5 + "Buzz");
+                System.out.println(i + " Buzz");
             }
             else{
                 System.out.println(i);
             }
-
         }
+
+        System.out.println("So long and thanks for all the fish");
+
     }
 }
