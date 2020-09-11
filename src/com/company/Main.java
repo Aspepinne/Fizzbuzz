efd3fa739d4e7704e48ce3056883404dc85bd7a8
Maria Hermansson <maria.hermansson@student.kyh.se>
2020-09-11 23:11:13 +0200

    Small adjustments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,20 +5,19 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 1; i < 101; i++) {
 
-            if (i == 42){
-                System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
+            if (i == 42) {
+                System.out.println( i + " Answer to the Ultimate Question of Life, the Universe, and Everything");
             }
-            else if (i % 3 == 0 && i%5 == 0){
-                
+            else if (i % 3 == 0 && i%5 == 0) {
                 System.out.println( i + " Fizzbuzz");
             }
-            else if (i % 3 == 0){
-                System.out.println(i + " Fizz");
+            else if (i % 3 == 0) {
+                System.out.println( i + " Fizz");
             }
             else if (i % 5 == 0) {
-                System.out.println(i + " Buzz");
+                System.out.println( i + " Buzz");
             }
-            else{
+            else {
                 System.out.println(i);
             }
         }
